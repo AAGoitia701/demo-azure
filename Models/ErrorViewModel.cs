277916c4d2d924221e25256ab3f9ba16f4ddc4ac
=AAGoitia701 <aagoitia701@gmail.com>
2024-06-12 10:36:48 +0200

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DemoAzureAAGR.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
